cef342b7895c7693c630befac7cf1e6b507cd706
Luis Moreno <morenol@users.noreply.github.com>
2021-04-09 12:02:41 -0400

    feat: Add enrollment related signals

--- openedx_events/signals/auth/v1/__init__.py
+++ openedx_events/signals/auth/v1/__init__.py
@@ -4,4 +4,5 @@ openedx_events.signals.auth.v1 module.
 
 from django.dispatch import Signal
 
+# Signal that fires when a user register in the platform
 REGISTER_USER = Signal(providing_args=["user", "registration"])
---
+++ openedx_events/signals/enrollment/__init__.py
@@ -0,0 +1,3 @@
+"""
+Module for enrollment related signals.
+"""
