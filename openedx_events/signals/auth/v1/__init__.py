3b4f3de93a53bd76b97688fe6c08cc36ec796284
Luis Moreno <morenol@users.noreply.github.com>
2021-04-14 11:09:52 -0400

    Add post login signal

--- openedx_events/signals/auth/v1/__init__.py
+++ openedx_events/signals/auth/v1/__init__.py
@@ -6,3 +6,6 @@ from django.dispatch import Signal
 
 # Signal that fires when a user register in the platform
 REGISTER_USER = Signal(providing_args=["user", "registration"])
+
+# Signal that fires when a user logins in the platform
+LOGIN_USER = Signal(providing_args=["user", "site"])
