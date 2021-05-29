09ad2f0207677ec4dad3733c9fdbaa1226bb1893
mariagrimaldi <maria.grimaldi@edunext.co>
2021-05-28 20:27:21 -0400

    test: testing attr package

---
+++ openedx_events/data.py
@@ -0,0 +1,85 @@
+import attr
+
+from opaque_keys.edx.keys import CourseKey
+from typing import Dict, FrozenSet, List, Optional
+
+
+import re
+
+# Make a regular expression
+# for validating an Email
+regex = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$'
+
+
+@attr.s(frozen=True, auto_attribs=True)
+class StudentData:
+    """
+    Attributes defined for Open edX User object.
+
+    Findings while testing:
+    1. Optional arguments have defaults
+    2. Default is defined after the equal sign
+    3. Dict[type1, type2]: {type1: type2}
+    4. Optional != than optional arguments
+    5. Optional is used when explicit values of None are allowed. Definition: Optional[X] is
+    equivalent to Union[X, None]
+    6. For advanced features use attr.ib not var: type
+    7. auto_attribs=True means attrs need type --> var: type
+    8. Type validation: https://www.attrs.org/en/stable/api.html#attr.validators.instance_of
+    9. Other validations: https://www.attrs.org/en/stable/api.html#validators
+    10. Add different type to username: nothing happens (see 12)
+    11. frozen=True creates immutable objects. Use case: Sometimes you have instances that shouldn’t be
+    changed after instantiation
+    12. Please note that types – however added – are only metadata that can be queried from the class and they aren’t
+    used for anything out of the box!
+    """
+    username: str
+    email: str
+    profile_meta: Dict[str, str]
+    is_active: bool
+    fullname: Optional[str]
+
+@attr.s(frozen=True)
+class StudentDataV2:
+    """
+    Attributes defined for Open edX User object.
+    """
+    profile_meta = attr.ib(factory=dict, type=Dict[str, str])
+
+
+@attr.s(frozen=True)
+class StudentDataV3:
+    """
+    Attributes defined for Open edX User object.
+    """
+    username = attr.ib(type=str, kw_only=True)
+    email = attr.ib(type=str, kw_only=True)
+    profile_meta = attr.ib(factory=dict, type=Dict[str, str], kw_only=True)
+    is_active = attr.ib(type=bool, kw_only=True)
+
+    @email.validator
+    def _email_validator(self, attribute, value):
+        if not re.search(regex, value):
+            raise ValueError("email must be valid.")
+
+    @is_active.default
+    def _is_active_default(self):
+        return True
+
+@attr.s(frozen=True, auto_attribs=True)
+class CourseData:
+    """
+    Attributes defined for Open edX course object.
+    """
+    course_key: CourseKey
+    display_name: str
+
+
+@attr.s(frozen=True, auto_attribs=True)
+class EnrollmentData:
+    """
+    Attributes defined for Open edX Course Enrollment object.
+    """
+    student: StudentData
+    course: CourseData
+    mode: str
---
+++ openedx_events/test_attr.py
@@ -0,0 +1,56 @@
+import attr
+
+from openedx_events.data import *
+from django.contrib.auth.models import User
+
+
+# Creating student student data
+user = User.objects.get(id=1)
+dict_student = [
+    user.username,
+    user.email,
+    user.profile.meta,
+    False,
+    'Fullname',
+]
+student_data = StudentData(*dict_student)
+print(student_data)
+# RESULT: StudentData(username='ecommerce_worker', email='ecommerce_worker@example.com', profile_meta='', is_active=False, fullname='Fullname')
+print(attr.asdict(student_data))
+# RESULT: {'username': 'ecommerce_worker', 'email': 'ecommerce_worker@example.com', 'profile_meta': '', 'is_active': False, 'fullname': 'Fullname'}
+dict_student_v2 = StudentDataV2()
+print(dict_student_v2)
+# RESULT: StudentDataV2(profile_meta={})
+dict_student_v2.profile_meta['ci'] = 123467
+print(dict_student_v2)
+# RESULT: StudentDataV2(profile_meta={'ci': 123467})
+# ERROR: dict_student_v2.profile_meta = 123467
+#   File "/edx/src/edxapp/attrs/src/attr/_make.py", line 628, in _frozen_setattrs
+#    raise FrozenInstanceError()
+# attr.exceptions.FrozenInstanceError
+student_data_v3 = StudentDataV3(
+    username='username',
+    email='email@gmail.com',
+    profile_meta={'ci': 123456789},
+    is_active=True,
+)
+print(student_data_v3)
+# ERROR: email validation
+# student_data_v3 = StudentDataV3(
+#    username='username',
+#    email='email@gmail.com',
+#    profile_meta={'ci': 123456789},
+#    is_active=True,
+#)
+#   raise ValueError("email must be valid.")
+#    ValueError: email must be valid.
+# StudentDataV3(username='username', email='email', profile_meta={'ci': 123456789}, is_active=True)
+# ERROR: missing keyword argument:
+# student_data_v3 = StudentDataV3(
+#    'username',
+#    email='email',
+#    profile_meta={'ci': 123456789},
+#    is_active=True,
+#)
+#   File "<string>", line 46, in <module>
+# TypeError: __init__() takes 1 positional argument but 2 positional arguments (and 3 keyword-only arguments) were given
