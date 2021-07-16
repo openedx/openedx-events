2fba7ae850ae5e0cb6f56026ec17d867bac23d94
mariagrimaldi <maria.grimaldi@edunext.co>
2021-07-16 11:41:23 -0400

    feat: add definition for learning subdomain related events

--- openedx_events/learning/signals.py
+++ openedx_events/learning/signals.py
@@ -7,3 +7,113 @@ conventions specified in docs/decisions/0002-events-naming-and-versioning.rst
 They also must comply with the payload definition specified in
 docs/decisions/0003-events-payload.rst
 """
+
+from openedx_events.learning.data import CertificateData, CohortData, CourseEnrollmentData, UserData
+from openedx_events.tooling import OpenEdxPublicSignal
+
+# .. event_type: org.openedx.learning.student.registration.completed.v1
+# .. event_name: STUDENT_REGISTRATION_COMPLETED
+# .. event_description: emitted when the user registration process in the LMS is completed.
+# .. event_data: UserData
+STUDENT_REGISTRATION_COMPLETED = OpenEdxPublicSignal(
+    event_type="org.openedx.learning.student.registration.completed.v1",
+    data={
+        "user": UserData,
+    }
+)
+
+
+# .. event_type: org.openedx.learning.auth.session.login.completed.v1
+# .. event_name: SESSION_LOGIN_COMPLETED
+# .. event_description: emitted when the user's login process in the LMS is completed.
+# .. event_data: UserData
+SESSION_LOGIN_COMPLETED = OpenEdxPublicSignal(
+    event_type="org.openedx.learning.auth.session.login.completed.v1",
+    data={
+        "user": UserData,
+    }
+)
+
+
+# .. event_type: org.openedx.learning.course.enrollment.created.v1
+# .. event_name: COURSE_ENROLLMENT_CREATED
+# .. event_description: emitted when the user's enrollment process is completed.
+# .. event_data: CourseEnrollmentData
+COURSE_ENROLLMENT_CREATED = OpenEdxPublicSignal(
+    event_type="org.openedx.learning.course.enrollment.created.v1",
+    data={
+        "enrollment": CourseEnrollmentData,
+    }
+)
+
+
+# .. event_type: org.openedx.learning.course.enrollment.changed.v1
+# .. event_name: COURSE_ENROLLMENT_CHANGED
+# .. event_description: emitted when the user's enrollment update process is completed.
+# .. event_data: CourseEnrollmentData
+COURSE_ENROLLMENT_CHANGED = OpenEdxPublicSignal(
+    event_type="org.openedx.learning.course.enrollment.changed.v1",
+    data={
+        "enrollment": CourseEnrollmentData,
+    }
+)
+
+
+# .. event_type: org.openedx.learning.course.unenrollment.completed.v1
+# .. event_name: COURSE_ENROLLMENT_CHANGED
+# .. event_description: emitted when the user's unenrollment process is completed.
+# .. event_data: CourseEnrollmentData
+COURSE_UNENROLLMENT_COMPLETED = OpenEdxPublicSignal(
+    event_type="org.openedx.learning.course.unenrollment.completed.v1",
+    data={
+        "enrollment": CourseEnrollmentData,
+    }
+)
+
+
+# .. event_type: org.openedx.learning.certificate.created.v1
+# .. event_name: CERTIFICATE_CREATED
+# .. event_description: emitted when the user's certificate creation process is completed.
+# .. event_data: CertificateData
+CERTIFICATE_CREATED = OpenEdxPublicSignal(
+    event_type="org.openedx.learning.certificate.created.v1",
+    data={
+        "certificate": CertificateData,
+    }
+)
+
+
+# .. event_type: org.openedx.learning.certificate.changed.v1
+# .. event_name: CERTIFICATE_CHANGED
+# .. event_description: emitted when the user's certificate update process is completed.
+# .. event_data: CertificateData
+CERTIFICATE_CHANGED = OpenEdxPublicSignal(
+    event_type="org.openedx.learning.certificate.changed.v1",
+    data={
+        "certificate": CertificateData,
+    }
+)
+
+
+# .. event_type: org.openedx.learning.certificate.revoked.v1
+# .. event_name: CERTIFICATE_REVOKED
+# .. event_description: emitted when the user's certificate annulation process is completed.
+# .. event_data: CertificateData
+CERTIFICATE_REVOKED = OpenEdxPublicSignal(
+    event_type="org.openedx.learning.certificate.revoked.v1",
+    data={
+        "certificate": CertificateData,
+    }
+)
+
+
+# .. event_type: org.openedx.learning.cohort_membership.changed.v1
+# .. event_name: COHORT_MEMBERSHIP_CHANGED
+# .. event_description: emitted when the user's cohort update is completed.
+# .. event_data: CohortData
+COHORT_MEMBERSHIP_CHANGED = OpenEdxPublicSignal(
+    event_type="org.openedx.learning.cohort_membership.changed.v1",
+    data={
+        "cohort": CohortData,
+    }
+)
