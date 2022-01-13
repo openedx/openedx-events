0c310a1edb6d519c1fa42c2c03f39ba64207eb7d
Manjinder Singh <jinder1s@gmail.com>
2022-01-13 11:49:04 -0500

    doc: added more descriptions

--- openedx_events/enterprise/data.py
+++ openedx_events/enterprise/data.py
@@ -10,24 +10,29 @@ import attr
 @attr.s(frozen=True)
 class TrackingEvent:
     """
-    License events to be put on event bus.
-
-    TODO: figure out proper description
+    Event to track changes to subscriptsion.License records.
 
     Arguments:
-        license_uuid (str):
+        license_uuid (str): The UUID linked to this license and storing in the License Manager DB.
+                            This value is always present.
         license_activation_key (str):
-        previous_license_uuid (str):
-        assigned_date (str):
-        activation_date (str):
-        assigned_lms_user_id (str):
-        assigned_email (str):
-        expiration_processed (bool):
+        previous_license_uuid (str): The UUID linked to the previous license that this license is a renewal for.
+                                     Non-empty only on after the edx.server.license-manager.license-lifecycle.renewed event has occurred.
+        assigned_date (str): Formatted ISO-8601 Date String representing the date this license was most recently assigned to the assigned_email.
+                             May be empty.
+        activation_date (str): Formatted ISO-8601 Date String this license was most recently activated for the assigned_email.
+                               May be empty.
+        assigned_lms_user_id (str): The LMS User id of the user that this license 'belongs to'.
+                                    May be empty if this license is not activated yet.
+        assigned_email (str): The email assigned to this license.
+                              Will be empty if this license is not assigned to a learner yet.
+        expiration_processed (bool): Boolean value of License 'expiration_processed' field, may be True or False.
+                                     True means a license is expired.
         auto_applied (bool):
         enterprise_customer_uuid (str):
         enterprise_customer_slug (str):
         enterprise_customer_name (str):
-        customer_agreement_uuid (str):
+        customer_agreement_uuid (str): The active Customer Agreement UUID that this license is linked to.
     """
 
     license_uuid = attr.ib(type=str)
