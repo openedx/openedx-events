adbaef0ed14aac9a022eb349555e22dac469bccd
Rebecca Graber <rgraber@edx.org>
2022-07-22 09:23:48 -0400

    fixup!: lint

--- openedx_events/content_authoring/__init__.py
+++ openedx_events/content_authoring/__init__.py
@@ -2,5 +2,5 @@
 Package where events related to the ``content_authoring`` subdomain are implemented.
 
 The ``content_authoring`` subdomain corresponds to {Architecture Subdomain} defined in OEP-41
-(https://open-edx-proposals.readthedocs.io/en/latest/architectural-decisions/oep-0041-arch-async-server-event-messaging.html).
+(https://open-edx-proposals.readthedocs.io/en/latest/architectural-decisions/oep-0041-arch-async-server-event-messaging.html).  # pylint: disable=line-too-long
 """
