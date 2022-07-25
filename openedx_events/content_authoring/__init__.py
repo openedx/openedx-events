1e0903338ff08e5fc709a8eec10bd380aa4d4772
Rebecca Graber <rgraber@edx.org>
2022-07-25 12:02:20 -0400

    fixup!: seriously

--- openedx_events/content_authoring/__init__.py
+++ openedx_events/content_authoring/__init__.py
@@ -1,8 +1,7 @@
 """
 Package where events related to the ``content_authoring`` subdomain are implemented.
 """
-# Comment broken up to deal with line-length suppression for the long URL
+# Comment broken up to deal with line-length issues with the long URL
 
 # The ``content_authoring`` subdomain corresponds to {Architecture Subdomain} defined in OEP-41.
-# https://open-edx-proposals.readthedocs.io/en/latest/architectural-decisions/oep-0041-arch-async-server-event-messaging.html  # noqa: W501, pylint: disable=line-too-long
-
+# https://open-edx-proposals.readthedocs.io/en/latest/architectural-decisions/oep-0041-arch-async-server-event-messaging.html
