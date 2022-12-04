c744de0d64fff194d0c373e27f86cc4738ea489b
NikolayBorovenskiy <nikolay.borovenskiy@gmail.com>
2022-12-04 17:42:50 +0200

    feat: [EDXOLDMNG-218] Signals to save/delete certificate configuration for a course

--- openedx_events/content_authoring/signals.py
+++ openedx_events/content_authoring/signals.py
@@ -8,7 +8,12 @@ They also must comply with the payload definition specified in
 docs/decisions/0003-events-payload.rst
 """
 
-from openedx_events.content_authoring.data import CourseCatalogData, DuplicatedXBlockData, XBlockData
+from openedx_events.content_authoring.data import (
+    CertificateConfigData,
+    CourseCatalogData,
+    DuplicatedXBlockData,
+    XBlockData,
+)
 from openedx_events.tooling import OpenEdxPublicSignal
 
 # .. event_type: org.openedx.content_authoring.course.catalog_info.changed.v1
@@ -60,5 +65,30 @@ XBLOCK_DUPLICATED = OpenEdxPublicSignal(
     event_type="org.openedx.content_authoring.xblock.duplicated.v1",
     data={
         "xblock_info": DuplicatedXBlockData,
+    }
+)
+
+
+# .. event_type: org.openedx.content_authoring.course.certificate_config.changed.v1
+# .. event_name: COURSE_CERTIFICATE_CONFIG_CHANGED
+# .. event_description: Fired when a course certificate configuration changes in Studio.
+#      Warning: This event is currently incompatible with the event bus, list/dict cannot be serialized yet
+# .. event_data: CertificateConfigData
+COURSE_CERTIFICATE_CONFIG_CHANGED = OpenEdxPublicSignal(
+    event_type="org.openedx.content_authoring.course.certificate_config.changed.v1",
+    data={
+        "certificate_config": CertificateConfigData,
+    }
+)
+
+# .. event_type: org.openedx.content_authoring.course.certificate_config.deleted.v1
+# .. event_name: COURSE_CERTIFICATE_CONFIG_DELETED
+# .. event_description: Fired when a course certificate configuration deletes in Studio.
+#      Warning: This event is currently incompatible with the event bus, list/dict cannot be serialized yet
+# .. event_data: CertificateConfigData
+COURSE_CERTIFICATE_CONFIG_DELETED = OpenEdxPublicSignal(
+    event_type="org.openedx.content_authoring.course.certificate_config.deleted.v1",
+    data={
+        "certificate_config": CertificateConfigData,
     }
 )
