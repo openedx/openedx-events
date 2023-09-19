b5f6bf79a80fd8782edd04cb515dabaa0b1f9629
ilee2u <ilee@2u.com>
2023-09-19 16:07:36 -0400

    feat: fixed language of signal

--- openedx_events/learning/signals.py
+++ openedx_events/learning/signals.py
@@ -258,19 +258,19 @@ EXAM_ATTEMPT_RESET = OpenEdxPublicSignal(
 # .. event_name: ADD_COURSE_STAFF_ROLE
 # .. event_description: Assigns the "Course Staff" role to a user.
 # .. event_data: CourseStaffData
-ADD_COURSE_STAFF_ROLE = OpenEdxPublicSignal(
-    event_type="org.openedx.learning.user.add.course.staff.role.v1",
+COURSE_STAFF_ROLE_ADDED = OpenEdxPublicSignal(
+    event_type="org.openedx.learning.user.course.staff.role.added.v1",
     data={
         "course_staff_data": CourseStaffData,
     }
 )
 
-# .. event_type: org.openedx.learning.user.remove.course.staff.role.v1
-# .. event_name: REMOVE_COURSE_STAFF_ROLE
+# .. event_type: org.openedx.learning.user.course.staff.role.removed.v1
+# .. event_name: COURSE_STAFF_ROLE_REMOVED
 # .. event_description: Removes the "Course Staff" role from a user.
 # .. event_data: CourseStaffData
-REMOVE_COURSE_STAFF_ROLE = OpenEdxPublicSignal(
-    event_type="org.openedx.learning.user.remove.course.staff.role.v1",
+COURSE_STAFF_ROLE_REMOVED = OpenEdxPublicSignal(
+    event_type="org.openedx.learning.user.course.staff.role.removed.v1",
     data={
         "course_staff_data": CourseStaffData,
     }
