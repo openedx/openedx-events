4800d4af47fb9363bd46dc34fc69c79574397556
Cristhian Garcia <crisgarta8@gmail.com>
2023-10-30 14:21:03 -0500

    fix: update event type to follow conventions

--- openedx_events/analytics/signals.py
+++ openedx_events/analytics/signals.py
@@ -11,12 +11,12 @@ docs/decisions/0003-events-payload.rst
 from openedx_events.analytics.data import TrackingLogData
 from openedx_events.tooling import OpenEdxPublicSignal
 
-# .. event_type: org.openedx.analytics.event_tracking.emitted.v1
+# .. event_type: org.openedx.analytics.tracking.event.emitted.v1
 # .. event_name: TRACKING_EVENT_EMITTED
 # .. event_description: emitted when a tracking log is created.
 # .. event_data: TrackingLogData
 TRACKING_EVENT_EMITTED = OpenEdxPublicSignal(
-    event_type="org.openedx.analytics.event_tracking.emitted.v1",
+    event_type="org.openedx.analytics.tracking.event.emitted.v1",
     data={
         "tracking_log": TrackingLogData,
     }
