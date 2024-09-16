7b6f46abb7d6d094259ac0f08c6fa8f608ea9923
Maria Grimaldi <maria.grimaldi@edunext.co>
2024-09-16 18:35:42 +0200

    build: upgrade python version in all CI/CD workflows and implementation

--- setup.py
+++ setup.py
@@ -148,7 +148,7 @@ setup(
     ],
     include_package_data=True,
     install_requires=load_requirements('requirements/base.in'),
-    python_requires=">=3.8",
+    python_requires=">=3.11",
     license="Apache 2.0",
     zip_safe=False,
     keywords='Python edx',
