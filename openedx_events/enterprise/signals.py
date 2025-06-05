c39e55e66c32aded5823aa498125a558551291f8
Jesper Hodge <jhodge@outlook.de>
2025-06-05 12:31:15 -0400

    fix: lint

--- openedx_events/enterprise/signals.py
+++ openedx_events/enterprise/signals.py
@@ -9,10 +9,10 @@ docs/decisions/0003-events-payload.rst
 """
 
 from openedx_events.enterprise.data import (
-  LearnerCreditEnterpriseCourseEnrollment,
-  LedgerTransaction,
-  SubsidyRedemption,
-  EnterpriseGroup
+    EnterpriseGroup,
+    LearnerCreditEnterpriseCourseEnrollment,
+    LedgerTransaction,
+    SubsidyRedemption,
 )
 from openedx_events.tooling import OpenEdxPublicSignal
 
